55db74acbfc3471d309758e43ce235c841be1b7d
Michal Kuchta <niximor@gmail.com>
2017-03-08 16:26:02 +0100

    Added open-port and df checks.

--- collector/src/sysmon-checker.py
+++ collector/src/sysmon-checker.py
@@ -6,8 +6,9 @@ import socket
 import syslog
 import subprocess
 import re
-
+import os
 import time
+import traceback
 
 
 SYSMON_ADDRESS = "https://sysmon.lan.gcm.cz"
@@ -110,11 +111,139 @@ def check_ping(check):
     }
 
 
+def check_port_open(check):
+    alerts = []
+    readings = {}
+
+    s_family = socket.AF_INET
+
+    all_ok = True
+
+    v6 = check["params"].get("IPV6", "0")
+    if v6 == "1":
+        s_family = socket.AF_INET6
+    elif v6 not in ("0", "1"):
+        alerts.append({
+            "type": "bad_param",
+            "data": {
+                "name": "IPV6",
+                "expected": "0 or 1"
+            }
+        })
+        all_ok = False
+
+    s_type = socket.SOCK_STREAM
+
+    address = check["params"].get("ADDRESS", "127.0.0.1")
+
+    try:
+        port = int(check["params"]["PORT"])
+        if port <= 0 or port > 65535:
+            raise ValueError("port outside range")
+    except (KeyError, ValueError) as e:
+        alerts.append({
+            "type": "bad_param",
+            "data": {
+                "name": "PORT",
+                "expected": "int"
+            }
+        })
+        all_ok = False
+
+    try:
+        timeout = float(check["params"].get("TIMEOUT", "0"))
+        if timeout < 0.0:
+            raise ValueError("timeout must be positive")
+        elif timeout < 0.0001:
+            timeout = None
+    except ValueError as e:
+        alerts.append({
+            "type": "bad_param",
+            "data": {
+                "name": "TIMEOUT",
+                "expected": "float"
+            }
+        })
+        all_ok = False
+
+    if all_ok:
+        try:
+            s = socket.socket(s_family, s_type)
+            s.settimeout(timeout)
+            s.connect((address, port))
+            s.close()
+        except socket.error as e:
+            alerts.append({
+                "type": "port_check_failed",
+                "data": {
+                    "errno": e.errno,
+                    "strerror": e.strerror
+                }
+            })
+
+    return {
+        "alerts": alerts,
+        "readings": readings
+    }
+
+
+def check_df(check):
+    alerts = []
+    readings = {}
+
+    try:
+        mountpoint = check["params"]["MOUNTPOINT"]
+
+        stat = os.statvfs(mountpoint)
+
+        total_size = stat.f_frsize * stat.f_blocks
+        free_space = stat.f_frsize * stat.f_bavail
+
+        readings["size_bytes"] = total_size
+        readings["free_bytes"] = free_space
+        readings["free_percent"] = free_space * 100.0 / total_size
+
+        try:
+            if readings["free_percent"] < float(check["params"].get("ALERT_THRESHOLD", 0)):
+                alerts.append({
+                    "type": "low_disk_space",
+                    "data": {
+                        "mountpoint": mountpoint,
+                        "free_percent": readings["free_percent"],
+                        "free_bytes": readings["free_bytes"],
+                        "size_bytes": readings["size_bytes"]
+                    }
+                })
+        except ValueError as e:
+            alerts.append({
+                "type": "bad_param",
+                "data": {
+                    "name": "ALERT_THRESHOLD",
+                    "expected": "float"
+                }
+            })
+
+    except KeyError as e:
+        alerts.append({
+            "type": "bad_param",
+            "data": {
+                "name": str(e),
+                "expected": "Mount point"
+            }
+        })
+
+    return {
+        "alerts": alerts,
+        "readings": readings
+    }
+
+
 def process_check(check):
     check_calls = {
         "ping": check_ping,
         "http": check_http,
-        "port-open": None,
+        "port-open": check_port_open,
+        "df": check_df
     }
 
     type_name = check.get("type")
@@ -154,6 +283,8 @@ def process_check(check):
             ]
         })
 
+        syslog.syslog(syslog.LOG_ERR, traceback.format_exc())
+
     return out
 
 
@@ -176,10 +307,7 @@ def main():
             if r.status_code != 200:
                 syslog.syslog(syslog.LOG_ERR, r.text)
     except Exception as e:
-        import traceback
-        traceback.print_exc()
-
-        syslog.syslog(syslog.LOG_ERR, str(e))
+        syslog.syslog(syslog.LOG_ERR, traceback.format_exc())
 
     syslog.closelog()
 
